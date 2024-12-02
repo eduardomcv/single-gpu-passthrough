5a679d5b2849d2df36b34aa3d7262e1e39ad4fb0
Eduardo Vicente <eduardomcv42@gmail.com>
2024-12-02 19:14:35 +0000

    Update start.sh

--- etc/libvirt/hooks/qemu.d/win10-singlegpu/prepare/begin/start.sh
+++ etc/libvirt/hooks/qemu.d/win10-singlegpu/prepare/begin/start.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 set -x
 
+source "/etc/libvirt/hooks/kvm.conf"
+
 # Stop display manager
+systemctl --user -M eduardo@ stop plasma*
 systemctl stop display-manager
 
-# Unbind VTconsoles
+# Unbind VTconsoles: might not be needed
 echo 0 > /sys/class/vtconsole/vtcon0/bind
-echo 0 > /sys/class/vtconsole/vtcon1/bind
-
-# Unbind EFI-Framebuffer
-echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+# echo 0 > /sys/class/vtconsole/vtcon1/bind
 
 # Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
-sleep 2
-#
-# Unload AMD kernel module
+sleep 2 
+
+# Unbind EFI Framebuffer
+# echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+# Detach GPU devices from host
+virsh nodedev-detach $VIRSH_GPU_VIDEO
+virsh nodedev-detach $VIRSH_GPU_AUDIO
+
+# Unload kernel modules
 modprobe -r amdgpu
+modprobe -r snd_hda_intel
 
-# Unbind the GPU from display driver
-virsh nodedev-detach pci_0000_03_00_0
-virsh nodedev-detach pci_0000_03_00_1
-
-# Unbind Audio devices
-virsh nodedev-detach pci_0000_00_1f_0
-virsh nodedev-detach pci_0000_00_1f_3
-virsh nodedev-detach pci_0000_00_1f_4
-virsh nodedev-detach pci_0000_00_1f_5
-
-# Load VFIO Kernel Module  
+# Load vfio module
 modprobe vfio
-modprobe vfio_pci  
+modprobe vfio_pci
 modprobe vfio_iommu_type1
-
