e0d12d47c709528f7c0c8cd26eb7740a26bc58f6
Eduardo Vicente <eduardomcv42@gmail.com>
2024-12-29 20:45:14 +0000

    add intel pci devices and update comments

--- hooks/qemu.d/win10/prepare/begin/start.sh
+++ hooks/qemu.d/win10/prepare/begin/start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
 set -x
 
 source "/etc/libvirt/hooks/kvm.conf"
 
-# Stop display manager
+# Stop kde plasma services
 systemctl --user -M eduardo@ stop plasma*
+
+# Stop display manager
 systemctl stop display-manager
 
-# Unbind VTconsoles: might not be needed
+# Unbind VTconsoles
 echo 0 >/sys/class/vtconsole/vtcon0/bind
-# echo 0 > /sys/class/vtconsole/vtcon1/bind
+echo 0 >/sys/class/vtconsole/vtcon1/bind
 
 # Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
 sleep 2
@@ -17,14 +20,19 @@ sleep 2
 # Unbind EFI Framebuffer
 # echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-# Detach GPU devices from host
+# Detach devices from host
 virsh nodedev-detach $VIRSH_GPU_VIDEO
 virsh nodedev-detach $VIRSH_GPU_AUDIO
+virsh nodedev-detach $VIRSH_ONBOARD_CARD
+virsh nodedev-detach $VIRSH_ONBOARD_AUDIO
+virsh nodedev-detach $VIRSH_ONBOARD_SMBUS
+virsh nodedev-detach $VIRSH_ONBOARD_SPI
 
 # Unload kernel modules
 modprobe -r amdgpu
+modprobe -r snd_hda_intel
 
-# Load vfio module
+# Load vfio modules
 modprobe vfio_pci
 modprobe vfio
 modprobe vfio_iommu_type1
--- hooks/qemu.d/win10/release/end/stop.sh
+++ hooks/qemu.d/win10/release/end/stop.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
+
 set -x
 
 source "/etc/libvirt/hooks/kvm.conf"
 
-# Attach GPU devices to host
+# Re-attach devices to host
 virsh nodedev-reattach $VIRSH_GPU_VIDEO
 virsh nodedev-reattach $VIRSH_GPU_AUDIO
+virsh nodedev-reattach $VIRSH_ONBOARD_CARD
+virsh nodedev-reattach $VIRSH_ONBOARD_AUDIO
+virsh nodedev-reattach $VIRSH_ONBOARD_SMBUS
+virsh nodedev-reattach $VIRSH_ONBOARD_SPI
 
-# Unload vfio module
+# Unload vfio modules
+modprobe -r vfio_iommu_type1
 modprobe -r vfio_pci
 modprobe -r vfio
-modprobe -r vfio_iommu_type1
 
-# Load AMD kernel module
+# Load kernel modules
 modprobe amdgpu
+modprobe snd_hda_intel
 
 # Rebind framebuffer to host
-# echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+# echo "efi-framebuffer.0" >/sys/bus/platform/drivers/efi-framebuffer/bind
 
-# Bind VTconsoles: might not be needed
+# Bind VTconsoles
 echo 1 >/sys/class/vtconsole/vtcon0/bind
-# echo 1 > /sys/class/vtconsole/vtcon1/bind
+echo 1 >/sys/class/vtconsole/vtcon1/bind
 
-# Restart Display Manager
+# Restart pipewire audio
+systemctl --user -M eduardo@ start pipewire
+systemctl --user -M eduardo@ start pipewire-pulse
+
+# Restart display manager
 systemctl start display-manager
